2f5e37f4d9248181fdc05918568b7e4c70502cae
Clinton McKay <ziesunkownemail@clueless.duh>
2014-02-07 14:59:26 -0700

    seperated the includes into a header file

---
+++ src/includes.hpp
@@ -0,0 +1,10 @@
+#ifndef INCLUDES_H 
+#define INCLUDES_H
+#include <iostream>
+#include <thread>
+#include <mutex>
+#endif
+
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,8 @@
-#include <iostream>
-#include <thread>
-#include <mutex>
+#include "includes.hpp"
 
-//using namespace std;
 int main(int argc, char *argv[]) {
     std::cout << "Hello World!" << std::endl;
 }
-
-
 
 
 
