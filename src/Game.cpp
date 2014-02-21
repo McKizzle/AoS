d32ec27886f3438fc1e9412c84795ecba9eba650
Clinton McKay <ziesunkownemail@clueless.duh>
2014-02-20 22:54:10 -0700

    The game class now displays and SDL window that uses an OpenGL context.

--- src/Game.cpp
+++ src/Game.cpp
@@ -3,12 +3,95 @@
 
 using namespace aos;
 
-Game::Game(Integrator * integrator)
+Game::Game() 
 {
-        
+    std::cout << "Created a game object\n"; 
 }
 
-Game::~Game() {
+Game::~Game()
+{
+    std::cout << "Destroyed a game object\n";  
+}
+
+int Game::init() 
+{
+    std::cout << "Initializing the environment for the game. \n";
+
+    // 1) Load the required data
+    //          In this case create a simple object that spins.
+     
+    // 2) Initialize the game data.
+    init_sdl();
+    init_gl();
+
+    // 3) TODO: Start the update loop.
+    render_loop();
+    // 4) TODO: Start the render loop.
+    update_loop();
+
+    return 1;
+}
+
+int Game::init_sdl() 
+{
+    if( SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        // Failed to initizlize SDL
+        return 1;
+    } 
+
+    sdl_window = SDL_CreateWindow(
+            "Asteroids on Steroids",
+            100, 100, 
+            screen_width, screen_height,
+            SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN
+    );
+    if( this->sdl_window == nullptr ) 
+    {
+        return 2;
+    }
+
+    return 1;
+}
+
+
+int Game::init_gl()
+{ 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3); // Request openGL 3
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1); // Set up double buffering
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24); // Set the color depth. 
+
+    // 3) Create an opengl Context.
+    sdl_gl_context = SDL_GL_CreateContext(sdl_window);
+
+    SDL_GL_SetSwapInterval(1); // Enable V-Sync
+}
+
+void Game::render_loop()
+{ 
+    //Clear to red background. 
+    glClearColor(1.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    SDL_GL_SwapWindow(sdl_window);
+    SDL_Delay(2000);
+
+    glClearColor(0.0, 1.0, 1.0 , 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    SDL_GL_SwapWindow(sdl_window);
+    SDL_Delay(2000);
     
+
+    glClearColor(0.0, 0.0, 1.0 , 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    SDL_GL_SwapWindow(sdl_window);
+    SDL_Delay(2000);
+
+    SDL_GL_DeleteContext(sdl_gl_context);
+    SDL_DestroyWindow(sdl_window);
+    SDL_Quit();    
 }
 
+void Game::update_loop()
+{
+
+}
--- src/Game.hpp
+++ src/Game.hpp
@@ -1,11 +1,18 @@
+//SDL Includes
+#include <SDL2/SDL.h>
+#ifdef __APPLE__
+#include <OpenGL/GL.h>
+#elif __linux__
+#include <GL/gl.h>
+#endif
+
 #ifndef GAME_HPP
 #define GAME_HPP
 #include "includes.hpp"
-#include "Physics.hpp"
-//#include "Renderer
 
 namespace aos {
-    /// \class Game game.hpp
+    /// \class Game Game.hpp
+    /// \brief Game management area.
     /// 
     /// Contains all of the code nessesary to control the game loops. 
     /// There will be three main loops. The integrator loop (updates object positions),
@@ -13,22 +20,25 @@ namespace aos {
     /// All three loops will access the objects in the game and update them approprietly. 
     class Game {
         public:
-            Game(Integrator * integrator/*, Renderer * renderer, Input * inputer*/);
+            Game();
             ~Game();
+            /// The init method calls all of the necessary code in order to setup 
+            ///  the game environment. If it fails to do so, then it will provide 
+            ///  an error code.
+            /// \return 0 on success otherwise return another number.
+            int init(); 
         private:
-            //Integrator * integrator;
-            //Renderer    renderer;
-            //Input       inputer;
-    };
-
-    /// \class Input input.hpp
-    class Input {
-        public:
-            Input();
-            ~Input();
-        private:
-            ///int input_handler =
-            
+            int screen_width = 512, screen_height = 512;
+            SDL_Window * sdl_window;
+            SDL_GLContext sdl_gl_context;
+            /// The render_loop runs in a thread. Its sole job is to call
+            ///  a renderable object.
+            void render_loop();
+            /// The update_loop runs in a thread. Its sole job is to call
+            ///  an updatable object.
+            void update_loop();
+            int init_gl();      ///< Initializes OpenGL for the game.
+            int init_sdl();     ///< Initializes SDL for the game.
     };
 }
 #endif
