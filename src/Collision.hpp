39ed00a2a592e11a0ce325d1c5d924f36a1bfedf
Clinton McKay <ziesunkownemail@clueless.duh>
2014-04-02 21:20:38 -0600

    added teh collision detection objects. Still inefficient. Need to fix this

--- src/Collision.hpp
+++ src/Collision.hpp
@@ -11,12 +11,24 @@ namespace aos
     class Collision
     {
         public:
+            Object *collider; ///< Object to monitor.  
+            std::vector< Object * > collideables;
             std::vector< Object * > bins;
 
-            Collision(Object * ref_obj);
+            Collision(Object * collider);
             ~Collision(); 
+            void add_collidable(Object * obj);
+            void add_collidables(std::vector< Object * > objs);
+            std::vector< double > test_collision(bool & collided); ///< Tests for a collision. Returns the intersection point. 
 
-            void test_collision(Object * obj);
+            // For now lets simply use these functions. 
+            static std::vector<double> test_object_collision(Object * obj1, Object * obj2, bool & collided);
+            static bool test_segment_collision(
+                    double p0_x, double p0_y, 
+                    double p1_x, double p1_y, 
+                    double p2_x, double p2_y, 
+                    double p3_x, double p3_y, 
+                    double *i_x, double *i_y);
     };
 }
 
