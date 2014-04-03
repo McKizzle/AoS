cedea7441fd5a606de83792272842e3b3209030c
Clinton McKay <ziesunkownemail@clueless.duh>
2014-04-02 21:19:49 -0600

    grid object added to the game

---
+++ src/Grid.cpp
@@ -0,0 +1,93 @@
+#include "Grid.hpp"
+
+
+namespace aos
+{
+Grid::Grid(double width, double height)
+{
+    float color_max = 255.0;
+    major_color[0] = 64.0 / color_max;
+    major_color[1] = 64.0 / color_max;
+    major_color[2] = 64.0 / color_max;
+
+    minor_color[0] = 32.0 / color_max;
+    minor_color[1] = 32.0 / color_max;
+    minor_color[2] = 32.0 / color_max;
+
+    this->width = width;
+    this->height = height;
+}
+Grid::~Grid(){}
+
+void Grid::render(Uint32 dt_ms, Uint32 time)
+{ 
+    const double x_max = width / 2.0;
+    const double x_min = -width / 2.0;
+    const double y_max = width / 2.0;
+    const double y_min = -width / 2.0;
+    const double x = (double) ((int)(obj_camera->x() / vertical_major_spacing)) * vertical_major_spacing;
+    const double y = (double) ((int)(obj_camera->y() / horizontal_major_spacing)) * horizontal_major_spacing;
+
+    glPushMatrix();
+    glTranslatef(x - camera->x(), y - camera->y(), 0.0);
+    glRotatef(state[HIND], 0, 0, 1);
+    glBegin(GL_LINES);  
+        double x1, y1, x2, y2;
+
+        glColor3f(minor_color[0], minor_color[1], minor_color[2]);
+        // Draw the minor x lines
+        for(int i = x_min; i <= x_max; i += vertical_minor_spacing)
+        {
+            x1 = i;
+            y1 = y_min;
+            x2 = i;
+            y2 = y_max;
+
+            glVertex2f((GLfloat) x1, (GLfloat) y1);
+            glVertex2f((GLfloat) x2, (GLfloat) y2);
+        }
+        // Draw the minor y lines
+        for(int i = y_min; i <= y_max; i += horizontal_minor_spacing)
+        { 
+             x1 = x_min;
+             y1 = i;
+             x2 = x_max;
+             y2 = i;
+
+            glVertex2f((GLfloat) x1, (GLfloat) y1);
+            glVertex2f((GLfloat) x2, (GLfloat) y2);
+        }
+        
+        glColor3f(color[0], color[1], color[2]);
+        // Draw the major x lines 
+        for(int i = x_min; i <= x_max; i += vertical_major_spacing)
+        {
+            x1 = i;
+            y1 = y_min;
+            x2 = i;
+            y2 = y_max;
+
+            glVertex2f((GLfloat) x1, (GLfloat) y1);
+            glVertex2f((GLfloat) x2, (GLfloat) y2);
+        }
+        // Draw the major y lines
+        for(int i = y_min; i <= y_max; i += horizontal_major_spacing)
+        { 
+             x1 = x_min;
+             y1 = i;
+             x2 = x_max;
+             y2 = i;
+
+            glVertex2f((GLfloat) x1, (GLfloat) y1);
+            glVertex2f((GLfloat) x2, (GLfloat) y2);
+        }
+    glEnd();
+    glPopMatrix();
+}
+
+void Grid::update(Uint32 dt_ms, Uint32 time)
+{
+    
+}
+
+}
---
+++ src/Grid.hpp
@@ -0,0 +1,36 @@
+#include "includes.hpp"
+
+
+#ifndef GRID_HPP
+#define GRID_HPP
+#include "Object.hpp"
+
+namespace aos
+{
+    class Grid: public Object
+    {
+        public:
+            Grid(double width, double height);
+            ~Grid();
+
+            Camera * obj_camera; ///< The Object the grid will 'follow'
+
+            virtual void render(Uint32 dt_ms, Uint32 time); ///< Inherited from Renderable
+            virtual void update(Uint32 dt_ms, Uint32 time); ///< Inherited from Updatable
+            
+            double width = 500;
+            double height = 500;
+                
+            std::vector< float > & major_color = color; /// Simply use the color for the major gridlines. 
+            std::vector< float > minor_color = {1.0, 1.0, 1.0, 1.0}; ///< The color of the object. 
+
+            double horizontal_major_spacing = 10;
+            double horizontal_minor_spacing = 5;
+            double vertical_major_spacing = 10;
+            double vertical_minor_spacing = 5;
+            
+    };
+}
+
+#endif
+
