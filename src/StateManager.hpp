12d661653a5d1385b1e77ca73f1b25e17000bdce
Clinton McKay <ziesunkownemail@clueless.duh>
2014-05-07 23:43:49 -0600

    is there libftgl2 on debian??

---
+++ src/StateManager.hpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+#ifndef COLLISION_HPP
+#define COLLISION_HPP
+
+#include "Systems.hpp"
+
+namespace aos
+{
+
+    class StateManager: public System
+    {
+        public:
+            StateManager();  
+            ~StateManager();
+    }
+}
+
