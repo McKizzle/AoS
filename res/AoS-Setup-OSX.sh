d6e1898684104c457fc74ef0371b8b605bcedc58
MrMcKizzle <ziesunkownemail@clueless.duh>
2014-05-19 16:56:45 -0600

    Check for brew installation.

--- res/AoS-Setup-OSX.sh
+++ res/AoS-Setup-OSX.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
 
+echo "Checking for homebrew."
+if ! type brew 2>/dev/null; then
+    echo "No brew installation found. Go to http://brew.sh/ and install brew with their installation command"
+    echo "Afterwards make sure to run `brew doctor` to make sure there will be no issues"
+    exit
+else
+    echo "Brew found, continuing the installation."
+fi
+
 brew install sdl2
 brew tap homebrew/versions
 brew install gcc # should install the newest version (gcc4.8)
